cba93c12f1937676acacd39cd0e4492408037cd7
Léo Jeanjean <leo.jeanjean@52-entertainment.com>
2022-04-14 09:42:40 +0200

    Add dot.env and utils

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./layout/**/*.{js,ts,jsx,tsx}',
+		'./utils/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}'
 	],
 	theme: {
