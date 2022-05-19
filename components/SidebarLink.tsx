bc7381d1bafb06026a423e77bb9e74e06d1570da
Geoffrey Morizot <gffr.mrzt@gmail.com>
2022-05-19 19:12:08 +0200

    refacto: Use next link tag instead of a tag

--- components/SidebarLink.tsx
+++ components/SidebarLink.tsx
@@ -1,27 +1,23 @@
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import classNames from '../utils/classNames'
 
 function SidebarLink({ children, href, icon }: any) {
+	
 	const router = useRouter()
-	console.log(router)
+
 	const classes = classNames(
 		'cursor-pointer rounded-md p-2 pr-24 flex',
 		router.asPath === href
 			? 'text-white bg-gray-900'
 			: 'text-gray-300 hover:bg-gray-700 hover:bg-gray-700 hover:text-white'
 	)
-	console.log(icon)
-
-	const handleClick = (e: any) => {
-		e.preventDefault()
-		router.push(href)
-	}
 
 	return (
 		<li className={''}>
-			<a href={href} onClick={handleClick}>
+			<Link href={href}>
 				<div className={classes}>{children}</div>
-			</a>
+			</Link>
 		</li>
 	)
 }
