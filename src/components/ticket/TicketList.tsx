6da6d47e41c41d449a1a1eef43f7a8286c346a1c
Hochart-dev <hochart.dev@gmail.com>
2022-09-02 15:51:48 +0200

    test8658 workflow

--- src/components/ticket/TicketList.tsx
+++ src/components/ticket/TicketList.tsx
@@ -8,7 +8,7 @@ const TicketList = ({ tickets }: ITicketsListProps) => (
 	<table className='TicketsList w-full '>
 		<thead className={'bg-gray-100'}>
 			<tr>
-				<td>NomSSATETOTOENCOREPLUSLONG2CESTVRAIMENTTROPLONG</td>
+				<td>NomSSATETOTOENCOREPLUSLONG2CESTVRAIMENTTROPLONGNONMAISVRAIMENT</td>
 				<td>Projet</td>
 				<td>Status</td>
 				<td>Assigné</td>
